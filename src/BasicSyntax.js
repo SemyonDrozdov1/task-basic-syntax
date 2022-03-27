c394aeeb03f8ebd631b3688d7a593bde793e9bc6
Дроздов Семён <s.drozdov2448@yandex.ru>
2022-03-27 20:57:15 +0300

    Задание по блоку тем Синтаксис

--- src/BasicSyntax.js
+++ src/BasicSyntax.js
@@ -4,5 +4,12 @@ export function romanToInteger(str) {
     Напишите код, преобразующий число из римской записи в арабскую.
     На вход вы получите строку, ответом должно быть число.
      */
+    let comparison = { I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000 };
+    let convert = Object.keys(comparison);
+    for (let i = 0; i < str.length; ++i) {
+        if (convert.indexOf(str[i]) < convert.indexOf(str[i + 1]))
+            result -= comparison[str[i]];
+        else result += comparison[str[i]];
+    }
     return result;
 }
